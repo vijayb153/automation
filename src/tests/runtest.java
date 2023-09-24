65eead243cbee265bd8aeab26f8ef626025112bd
vijaykumarb9343947713 <vijay.v13v@gmail.com>
2023-09-24 15:50:37 +0530

    add code

---
+++ src/tests/runtest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class runtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\selenium webdriver\\ChromeDriver\\chromedriver-win64\\chromedriver.exe");
+		
+		WebDriver dr= new ChromeDriver();
+		dr.get("https://googlechromelabs.github.io/chrome-for-testing/");
+	}
+
+}
